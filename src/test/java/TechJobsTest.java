66c7f4332b75e27b7aed646ab4226d5d7aaadd05
kelechi Obilor <obilorkelechi@yahoo.com>
2022-09-22 20:05:50 -0500

    second commit

--- src/test/java/TechJobsTest.java
+++ src/test/java/TechJobsTest.java
@@ -18,7 +18,7 @@ public class TechJobsTest {
     private static ByteArrayOutputStream baos;
 
     // set up an alternative output stream to capture output
-    // this needs to be done before every test so we don't contaminate test output
+    // this needs to be done before every test, so we don't contaminate test output
     @Before
     public void setUpOutputStream() {
         baos = new ByteArrayOutputStream();
@@ -42,7 +42,7 @@ public class TechJobsTest {
         String input = "0\n2\nBuzzbold\nx";
         String output = runProgramWithInput(input);
         String expected = getFileContents("src/test/resources/testPrintJobs.txt");
-        assertEquals(expected, output);
+        assertEquals(expected.replaceAll("\r\n?", "\n"), output.replaceAll("\r\n?", "\n"));
     }
 
     @Test
@@ -50,7 +50,7 @@ public class TechJobsTest {
         String input = "0\n2\nChicago\nx";
         String output = runProgramWithInput(input);
         String expected = getFileContents("src/test/resources/testPrintJobsNoResults.txt");
-        assertEquals(expected, output);
+        assertEquals(expected.replaceAll("\r\n?", "\n"), output.replaceAll("\r\n?", "\n"));
     }
 
     @Test
@@ -58,7 +58,7 @@ public class TechJobsTest {
         String input = "0\n0\nRuby\nx";
         String output = runProgramWithInput(input);
         String expected = getFileContents("src/test/resources/testFindByValue.txt");
-        assertEquals(expected, output);
+        assertEquals(expected.replaceAll("\r\n?", "\n"), output.replaceAll("\r\n?", "\n"));
     }
 
     @Test
@@ -66,7 +66,7 @@ public class TechJobsTest {
         String input = "0\n3\nnew YORk\nx";
         String output = runProgramWithInput(input);
         String expected = getFileContents("src/test/resources/testCaseInsensitiveSearch.txt");
-        assertEquals(expected, output);
+        assertEquals(expected.replaceAll("\r\n?", "\n"), output.replaceAll("\r\n?", "\n"));
     }
 
 
